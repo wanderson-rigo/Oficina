49b7385f7e254a4742b157b729b5f75fff1d6da2
wanderson-rigo <wanderson.rigo@gmail.com>
2023-10-09 14:25:18 -0300

    inicio

---
+++ oficina.py
@@ -0,0 +1,12 @@
+nome = "Wanderson"
+print(nome)
+
+sobrenome = "Rigo"
+print("Meu nome completo é",nome,sobrenome,",entendeu?")
+
+#letras em nome
+tamanhoNome = (len(nome))
+print("Meu nome tem",tamanhoNome,"letras")
+
+tamanhoSobrenome = (len(sobrenome))
+print("Meu sobrenome tem",tamanhoSobrenome,"letras")
